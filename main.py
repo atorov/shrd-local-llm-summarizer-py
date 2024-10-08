fe194989af8e5ec1568f82db4a6a30d1083aaf41
vpetrov-qb <vpetrov@quickbase.com>
2024-10-08 22:18:36 +0300

    initial implementation compleated

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello, world!")
+def save_text_to_file(text: str, file_path: str) -> None:
+    """
+    Save a string of text to a specified file path.
+
+    :param text: The text to be saved.
+    :param file_path: The path to the file to be written.
+    """
+    with open(file_path, 'w') as file:
+        file.write(text)
+
+
+save_text_to_file('Hello, world!', 'hello.txt')
